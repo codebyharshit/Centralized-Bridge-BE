45ecec7d2432365da09c6688bf6aab9e6b39a96d
codebyharshit <harshitagrawal048@gmail.com>
2023-06-12 08:52:50 +0530

    EventListener code updated

---
+++ config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+export const privateKey = process.env.privateKey;
+export const providerMatic = process.env.providerMatic;
+export const providerEth = process.env.providerEth; 
+export const TokenMaticAddress = process.env.TokenMaticAddress;
+export const TokenEthAddress = process.env.TokenEthAddress; 
+export const BridgeMaticAddress = process.env.BridgeMaticAddress; 
+export const BridgeEthAddress = process.env.BridgeEthAddress;
+
+
+
--- eventListener.js
+++ eventListener.js
@@ -1,75 +1,34 @@
 require("dotenv").config();
-const { ethers } = require("ethers");
-const { JsonRpcProvider } = require("ethers");
-const Token = require("./ABI/Token");
-const Bridge = require("./ABI/Bridge");
-const { parseEther } = require("ethers");
-const Web3 = require("web3");
-const web3 = new Web3();
+import { ethers } from "ethers";
+import { JsonRpcProvider } from "ethers";
+import Token from "./abi/token";
+import Bridge from "./abi/bridge";
+import {
+  privateKey,
+  providerMatic,
+  providerEth,
+  TokenMaticAddress,
+  TokenEthAddress,
+  BridgeMaticAddress,
+  BridgeEthAddress,
+} from "./config";
 
-const providerMatic = new JsonRpcProvider(
-  "https://polygon-mumbai.g.alchemy.com/v2/FUvWV-Ytjb0hH4R2T2Qa0k21bOwlyqWw"
-);
-
-const providerEth = new JsonRpcProvider(
-  "https://eth-sepolia.g.alchemy.com/v2/HugBgBUi34bleuAuHHP7WRE2cOkuTtoc"
-);
-
-const TokenMaticAddress = "0x627098Fcc59D1ac36899f12Acf0c5A9AAcBEd01b";
-
-const TokenEthAddress = "0x4e146e4a1B93035fde484db0E4F65563846Da3D9";
-
-const BridgeMaticAddress = "0x40AEFecb2dC5Ad4E96f7cFB437Ea4DC07862B68c";
-
-const BridgeEthAddress = "0xe2Af6495367DF41116e014baf500ac66D358Aae4";
+const providerMatic = new JsonRpcProvider(providerMatic);
+const providerEth = new JsonRpcProvider(providerEth);
 
 const contractBridgeEth = new ethers.Contract(
   BridgeEthAddress,
   Bridge,
   providerEth
 );
-
 const contractBridgeMatic = new ethers.Contract(
   BridgeMaticAddress,
   Bridge,
   providerMatic
 );
 
-// const eventsETH = async () => {
-//   contractBridgeEth.on("BRIDGE", async (from, amount) => {
-//     // console.log("Event received - From:", from);
-//     // console.log("Amount:", amount.toString());
-//     // console.log("Chain Name:", chainName.toString());
-//     sendTokenToWallet(from, amount)
-//       .then(() => {
-//         console.log("Tokens sent successfully");
-//       })
-//       .catch((error) => {
-//         console.error("Error sending tokens:", error);
-//       });
-//   });
-
-//   async function sendTokenToWallet(wallet, amount) {
-//     const walletWithProvider = new ethers.Wallet(privateKey, providerMatic);
-
-//     const tokenSmartContract = new ethers.Contract(
-//       TokenMaticAddress,
-//       Token,
-//       walletWithProvider
-//     );
-
-//     const transaction = await tokenSmartContract.transfer(wallet, amount);
-//     await transaction.wait();
-//     console.log("Tokens transferred successfully.");
-//   }
-// };
-
 const events = async () => {
   contractBridgeMatic.on("BRIDGE", async (from, amount) => {
-    // console.log("Event received - From:", from);
-    // console.log("Amount:", amount.toString());
-    // console.log("Chain Name:", chainName.toString());
-
     sendTokenEth(from, amount)
       .then(() => {
         console.log("Tokens sent successfully");
@@ -80,9 +39,6 @@ const events = async () => {
   });
 
   contractBridgeEth.on("BRIDGE", async (from, amount) => {
-    // console.log("Event received - From:", from);
-    // console.log("Amount:", amount.toString());
-    // console.log("Chain Name:", chainName.toString());
     sendTokenMatic(from, amount)
       .then(() => {
         console.log("Tokens sent successfully");
@@ -93,10 +49,7 @@ const events = async () => {
   });
 
   async function sendTokenEth(wallet, amount) {
-    const walletWithProvider = new ethers.Wallet(
-      process.env.privateKey,
-      providerEth
-    );
+    const walletWithProvider = new ethers.Wallet(privateKey, providerEth);
 
     const tokenSmartContract = new ethers.Contract(
       TokenEthAddress,
@@ -110,10 +63,7 @@ const events = async () => {
   }
 
   async function sendTokenMatic(wallet, amount) {
-    const walletWithProvider = new ethers.Wallet(
-      process.env.privateKey,
-      providerMatic
-    );
+    const walletWithProvider = new ethers.Wallet(privateKey, providerMatic);
 
     const tokenSmartContract = new ethers.Contract(
       TokenMaticAddress,
